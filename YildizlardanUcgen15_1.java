a51f7433aec6ba4498c9bf458c6a3e71e6d79b7d
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2025-02-01 05:59:17 +0300

    Create YildizlardanUcgen15_1.java

---
+++ YildizlardanUcgen15_1.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author Dogukan
+ */
+public class YildizlardanUcgen15_1 {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            for (int j = i; j < 9; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
