3e2f8a619b4a09471a49b5912558d852ad84e26d
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2025-02-01 05:27:10 +0300

    Create EkranaTarihiBasanKod3_1.java

---
+++ EkranaTarihiBasanKod3_1.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ *
+ * @author Dogukan
+ */
+public class EkranaTarihiBasanKod3_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Žuan Tarih:\t" + (new Date()));
+
+    }
+
+}
