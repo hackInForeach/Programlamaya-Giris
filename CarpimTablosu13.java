4624be834eae092a5ee39709cf41f6e93cdfaecb
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2025-02-01 05:52:57 +0300

    Create CarpimTablosu13.java

---
+++ CarpimTablosu13.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author Dogukan
+ */
+public class CarpimTablosu13 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print((j + 1) + " x " + (i + 1) + " = " + ((i + 1) * (j + 1))+"\t");
+            }
+            System.out.println("\t");
+        }
+
+    }
+
+}
