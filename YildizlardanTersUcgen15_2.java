bdb2344241bbabbf0297789a7235bfa274037d66
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2025-02-01 05:59:55 +0300

    Create YildizlardanTersUcgen15_2.java

---
+++ YildizlardanTersUcgen15_2.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author Dogukan
+ */
+public class YildizlardanTersUcgen15_2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            
+            for (int k = 0; k < i ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < 10; j++) {
+                System.out.print(" *");
+            }
+            
+            
+            System.out.println();
+        }
+
+    }
+}
