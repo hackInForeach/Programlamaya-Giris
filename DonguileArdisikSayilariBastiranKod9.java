b568b60d036d7cd2ebcaca7ab3fccc2085aabcd9
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2025-02-01 05:48:39 +0300

    Create DonguileArdisikSayilariBastiranKod9.java

---
+++ DonguileArdisikSayilariBastiranKod9.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Dogukan
+ */
+public class DonguileArdisikSayilariBastiranKod9 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("0");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("" + (i + 1));
+        }
+
+    }
+}
